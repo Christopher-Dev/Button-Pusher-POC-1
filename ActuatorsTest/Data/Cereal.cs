15fde69c5f6ba637fb6a8064111677a11252a35c
Michael Corello <mchlcorello871@Gmail.com>
2022-04-26 23:23:55 -0400

    Hope this works
    Elon bought twitter circa. 1912
    
    Co-Authored-By: Christopher Pinkus <59783738+Christopher-Dev@users.noreply.github.com>

---
+++ ActuatorsTest/Data/Cereal.cs
@@ -0,0 +1,42 @@
+﻿using System.IO.Ports;
+
+namespace ActuatorsTest.Data
+{
+    public class Cereal : ICereal
+    {
+        public void SendData()
+        {
+
+
+
+            //throw new NotImplementedException();
+        }
+
+
+        public void SendDataSample(byte bit)
+        {
+
+            var q = new List<string>();
+
+            //Local variable to check ports available
+            foreach (var item in SerialPort.GetPortNames())
+            {
+                q.Add(item);
+            }
+            
+            //Create the communication port with factory settings
+            SerialPort port = new SerialPort($"COM1", 9600, Parity.None, 8, StopBits.One);
+
+
+            port.Open();
+
+            port.Write(new byte[] {0x0A, 0xE2, 0xFF},0,3);
+
+            port.Close();
+        
+        }
+
+
+        
+    }
+}
---
+++ ActuatorsTest/Data/ICereal.cs
@@ -0,0 +1,8 @@
+﻿namespace ActuatorsTest.Data
+{
+    public interface ICereal
+    {
+        public void SendData();
+        public void SendDataSample(byte bit);
+    }
+}
